7ff627b51b1a9cdeac6538f944bc03dd228aa1a7
Dentix-hub <smartdentalclinicapp@gmail.com>
2026-08-29 05:06:27 +0300

    feat(chart): add dental anatomy registry scaffold

---
+++ frontend/src/features/clinical-chart/domain/index.js
@@ -0,0 +1,9 @@
+export {
+    DENTAL_ANATOMY_REGISTRY,
+    DENTITIONS,
+    PERMANENT_TOOTH_KEYS,
+    PRIMARY_TOOTH_KEYS,
+    TOOTH_TYPES,
+    getDentalAnatomy,
+} from './dentalAnatomyRegistry';
+
